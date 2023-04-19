5cb39a4175f52b7483687761a65078846c1983bd
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-19 08:51:50 +0100

    reduced test timeout time

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,8 @@ const config: PlaywrightTestConfig = {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'tests'
+	testDir: 'tests',
+	timeout: 3000
 };
 
 export default config;
