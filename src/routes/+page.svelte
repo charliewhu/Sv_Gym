e659c99cd89100b0ec9f68446d401821e9ddf69f
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-19 08:59:26 +0100

    navigation to create page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
-<button type="submit" aria-label="startWorkout" class="btn"> Start Workout </button>
+<form action="/create">
+	<button type="submit" aria-label="startWorkout" class="btn"> Start Workout </button>
+</form>
