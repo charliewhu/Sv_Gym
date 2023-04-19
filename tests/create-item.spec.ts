dfc3a92a5a53e68df8384346c594edeb238a4aa7
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-19 08:14:55 +0100

    added feature spec

---
+++ tests/create-item.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test';
+
+test('creating an item', async ({ page }) => {
+	// start a workout
+	// add exercises
+	// goto exercises
+	// add sets to exercise
+});
