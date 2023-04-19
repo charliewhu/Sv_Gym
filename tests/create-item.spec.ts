d0311edcd52e70980d5d02c6d1022f40ec5dd698
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-19 08:28:11 +0100

    added test code

--- tests/create-item.spec.ts
+++ tests/create-item.spec.ts
@@ -1,8 +1,29 @@
 import { expect, test } from '@playwright/test';
+import { prisma } from '../src/lib/server/prisma';
 
 test('creating an item', async ({ page }) => {
+	// variables
+	const exerciseName = 'Exercise 1';
+
+	// setup
+	await prisma.exercise.create({
+		data: {
+			name: exerciseName
+		}
+	});
+
 	// start a workout
+	await page.locator('button[aria-label="startWorkout"]').click();
+	await expect(page).toHaveURL('/workouts/1');
+
 	// add exercises
+	const exerciseDropdown = await page.getByRole('listitem');
+	exerciseDropdown.getByText(exerciseName);
+	await page.locator('button[aria-label="addExercise"]').click();
+	await expect(page.getByTestId('exerciseList')).toBeVisible();
+	await expect(page.getByTestId('exerciseListItem')).toHaveText(exerciseName);
+
 	// goto exercises
+
 	// add sets to exercise
 });
