6df4fadf6b438cc21d015ade92b0cd759284e76b
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-20 10:08:52 +0200

    added action for workout exercise

--- src/routes/workouts/[id]/+page.server.ts
+++ src/routes/workouts/[id]/+page.server.ts
@@ -1,6 +1,28 @@
 import { prisma } from '$lib/server/prisma';
+import { fail } from '@sveltejs/kit';
 
 export const load = async () => {
 	const exercises = await prisma.exercise.findMany();
 	return { exercises: exercises };
+};
+
+export const actions = {
+	default: async ({ request, params }) => {
+		const form = await request.formData();
+		const exerciseId = form.get('exercise');
+
+		// add exercise to workout
+		try {
+			await prisma.workoutexercise.create({
+				data: {
+					workoutId: params.id,
+					exerciseId: exerciseId
+				}
+			});
+		} catch (err) {
+			return fail(500, { message: 'could not add wokout exercise' });
+		}
+
+		return { status: 201 };
+	}
 };
--- src/routes/workouts/[id]/+page.svelte
+++ src/routes/workouts/[id]/+page.svelte
@@ -3,10 +3,12 @@
 </script>
 
 <form method="POST">
-	<select name="" id="" aria-label="exerciseDropdown">
+	<select name="exercise" aria-label="exerciseDropdown">
 		<option value="" />
 		{#each data.exercises as item}
-			<option data-testid="exerciseDropdownItem" value={item.name}>{item.name}</option>
+			<option data-testid="exerciseDropdownItem" value={item.id}>
+				{item.name}
+			</option>
 		{/each}
 	</select>
 	<button aria-label="addExercise" class="btn">Add</button>
