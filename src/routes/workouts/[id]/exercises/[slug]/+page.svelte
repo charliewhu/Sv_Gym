bee0b95054b6c6e88405794f02fb7be4a6a53871
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-21 19:16:55 +0200

    added form inputs

---
+++ src/routes/workouts/[id]/exercises/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page.params.slug}</title>
+</svelte:head>
+
+<form>
+	<input type="text" placeholder="Weight" />
+	<input type="text" placeholder="Reps" />
+	<input type="text" placeholder="RIR" />
+</form>
