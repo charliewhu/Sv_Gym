4b2fad813752d461ab3331c31f9a20a8e0d8ecb9
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-22 08:29:08 +0200

    added markup for setListItems

--- src/routes/workouts/[id]/exercises/[slug]/+page.svelte
+++ src/routes/workouts/[id]/exercises/[slug]/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+
+	export let data;
 </script>
 
 <svelte:head>
@@ -7,8 +9,18 @@
 </svelte:head>
 
 <form>
-	<input type="text" placeholder="Weight" />
-	<input type="text" placeholder="Reps" />
-	<input type="text" placeholder="RIR" />
+	<input type="text" name="weight" placeholder="Weight" />
+	<input type="text" name="reps" placeholder="Reps" />
+	<input type="text" name="rir" placeholder="RIR" />
 	<button aria-label="submitSet" class="btn">Add</button>
 </form>
+
+<table>
+	{#each data.sets as item}
+		<tr id="setListItem">
+			<td>{item.weight}</td>
+			<td>{item.reps}</td>
+			<td>{item.rir}</td>
+		</tr>
+	{/each}
+</table>
