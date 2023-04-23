e1ca3142d70a7b61042fb7af401f932b1bda5390
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-23 12:11:51 +0200

    created test cases

---
+++ tests/list-workouts.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from './base.ts';
+import { prisma } from '../src/lib/server/prisma.ts';
+
+test('listing workouts', async ({ page }) => {
+	// create workouts
+	await prisma.workout.create({ data: {} });
+	await prisma.workout.create({ data: {} });
+	const workouts = await prisma.workout.findMany();
+
+	// go to homepage
+	await page.goto('/');
+
+	// assert workouts are shown
+	const workoutItems = page.getByTestId('workoutListItem');
+	const count = await workoutItems.count();
+	expect(count).toEqual(workouts.length);
+
+	// click first workout
+	await workoutItems.first().click();
+
+	// assert link to workout page
+	await expect(page).toHaveURL('/workouts/1');
+});
