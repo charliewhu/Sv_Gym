73d3041cbad3cb60ad40aac71ab8be1a4a32a9c5
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-26 18:14:19 +0100

    added link to create exercise

--- src/routes/exercises/+page.svelte
+++ src/routes/exercises/+page.svelte
@@ -2,6 +2,8 @@
 	export let data;
 </script>
 
+<a href="/exercises/create" aria-label="createExercise" class="btn">Create New</a>
+
 {#each data.exercises as exercise}
 	<p data-testid="exerciseListItem">{exercise.name}</p>
 {/each}
