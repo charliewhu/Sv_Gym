28adbb4f8f96dac48ceb4d68d6813e9624675a7e
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-04-27 16:34:50 +0100

    nav and container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,40 @@
 	import '../app.css';
 </script>
 
-<slot />
+<nav class="navbar bg-base-300 mb-6">
+	<div class="container mx-auto max-w-5xl">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghost normal-case text-xl">Gym</a>
+		</div>
+		<div class="dropdown dropdown-end">
+			<button tabindex="0" class="btn btn-ghost btn-circle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h7"
+					/>
+				</svg>
+			</button>
+			<ul class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
+				<li>
+					<a href="/">Workouts</a>
+				</li>
+				<li>
+					<a href="/exercises">Exercises</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<div class="container mx-auto max-w-4xl">
+	<slot />
+</div>
