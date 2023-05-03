2b148cd5c8427f6ae0be3c635209fd80a0e54e63
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-03 19:44:15 +0100

    changed form

--- src/routes/exercises/create/+page.svelte
+++ src/routes/exercises/create/+page.svelte
@@ -1,4 +1,13 @@
-<form method="POST">
-	<input type="text" name="name" placeholder="Name" />
-	<button type="submit" aria-label="submitNewExercise" class="btn">Create</button>
+<form method="POST" class="text-center">
+	<div class="pb-6">
+		<input
+			type="text"
+			name="name"
+			placeholder="Name"
+			class="input input-bordered w-full max-w-xs"
+		/>
+	</div>
+	<div>
+		<button aria-label="submitSet" class="btn btn-primary w-2/12">Add</button>
+	</div>
 </form>
