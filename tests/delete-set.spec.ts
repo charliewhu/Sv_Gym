0b1061ff76c43cede7d64b4ed88df20375c8cbf0
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-08 18:27:30 +0100

    added test outline

---
+++ tests/delete-set.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from './base.ts';
+import { prisma } from '../src/lib/server/prisma.ts';
+
+test('deleting sets', async ({ page }) => {
+	// arrange (add sets to db)
+	// act: go to sets list page
+	// assert sets visible
+	// act: click delete button
+	// assert set deleted
+});
