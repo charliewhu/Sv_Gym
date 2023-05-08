bf185837a36f77d3b2df9f01fb0b77b0f4853014
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-08 18:36:22 +0100

    initial test spec

--- tests/delete-set.spec.ts
+++ tests/delete-set.spec.ts
@@ -3,8 +3,29 @@ import { prisma } from '../src/lib/server/prisma.ts';
 
 test('deleting sets', async ({ page }) => {
 	// arrange (add sets to db)
+	const workout = await prisma.workout.create({ data: {} });
+	const exercise = await prisma.exercise.create({ data: { name: 'exercise' } });
+	const workoutExercise = await prisma.workoutExercise.create({
+		data: { workoutId: workout.id, exerciseId: exercise.id }
+	});
+	await prisma.workoutExerciseSet.createMany({
+		data: [
+			{ workoutExerciseId: workoutExercise.id, weight: 100, reps: 5, rir: 2 },
+			{ workoutExerciseId: workoutExercise.id, weight: 110, reps: 8, rir: 3 }
+		]
+	});
+
 	// act: go to sets list page
-	// assert sets visible
+	await page.goto(`sets?exercise=${workoutExercise.id}`);
+
+	// assert: set delete buttons visible
+	let deleteBtns = page.getByTestId('deleteSetBtn');
+	await expect(deleteBtns).toHaveCount(2);
+
 	// act: click delete button
+	await page.getByTestId('deleteSetBtn').first().click();
+
 	// assert set deleted
+	deleteBtns = page.getByTestId('deleteSetBtn');
+	await expect(deleteBtns).toHaveCount(1);
 });
