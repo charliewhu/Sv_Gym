659824ea273b3eb60fbd11bf24fd1881a75b1059
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-09 20:07:50 +0100

    wrote test wireframe

---
+++ tests/delete-workout-exercise.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from './base.ts';
+import { prisma } from '../src/lib/server/prisma.ts';
+
+test('deleting sets', async ({ page }) => {
+	// arrange (add workout exercise to db)
+	// act: go to workout page
+	// assert: delete buttons visible
+	// act: click delete button
+	// assert workout exercise deleted
+});
