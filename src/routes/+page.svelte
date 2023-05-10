2106f9644fab9a2a9d8bafe13a59e924c2f63b37
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-10 19:16:58 +0100

    added delete button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 	import ListItem from '$lib/components/ListItem.svelte';
 
 	export let data;
 </script>
 
-<form method="POST" action="" class="text-center mb-6">
+<form action={`?/create`} method="POST" use:enhance class="text-center mb-6">
 	<button type="submit" aria-label="startWorkout" class="btn btn-primary">Start New Workout</button>
 </form>
 
@@ -14,7 +15,7 @@
 		<ListItem href={`workout-exercises?workoutId=${item.id}`} testId="workoutListItem">
 			<div class="flex flex-row justify-between items-center">
 				<div>{item.id}</div>
-				<form action={`${$page.url}?/delete`} method="POST">
+				<form action={`?/delete`} method="POST" use:enhance>
 					<input type="hidden" name="id" value={item.id} />
 					<button on:click|stopPropagation data-testid="deleteWorkoutBtn" class="btn btn-secondary">
 						Delete
