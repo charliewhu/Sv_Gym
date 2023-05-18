b61813ea2b980052de0032bcf983a6c0276053c1
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-18 17:21:26 +0100

    changed defaults

--- src/lib/components/SetsForm.svelte
+++ src/lib/components/SetsForm.svelte
@@ -4,6 +4,9 @@
 	export let action: string;
 	export let text: string;
 	export let label: string;
+	export let weight: number | null = null;
+	export let reps: number | null = null;
+	export let rir: number | null = null;
 </script>
 
 <form {action} method="POST" use:enhance class="text-center">
@@ -14,6 +17,7 @@
 				name="weight"
 				placeholder="Weight"
 				class="input input-bordered w-full max-w-xs flex flex-col"
+				value={weight}
 			/>
 		</div>
 		<div class="pb-4">
@@ -22,6 +26,7 @@
 				name="reps"
 				placeholder="Reps"
 				class="input input-bordered w-full max-w-xs"
+				value={reps}
 			/>
 		</div>
 		<div class="pb-4">
@@ -30,6 +35,7 @@
 				name="rir"
 				placeholder="RIR"
 				class="input input-bordered w-full max-w-xs"
+				value={rir}
 			/>
 		</div>
 	</div>
