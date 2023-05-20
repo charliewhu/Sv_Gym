1419ca70aefc2b0e3f8e0e203a949ccd77be808b
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-20 20:07:17 +0100

    changed folder structure

---
+++ tests/workout-exercise/create-set.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '../base.ts';
+import { prisma } from '../../src/lib/server/prisma.ts';
+
+test('creating an item', async ({ page }) => {
+	// variables
+	const exerciseNames = ['Bench Press', 'Exercise 2'];
+	const setInfo = {
+		weight: '100',
+		reps: '10',
+		rir: '2'
+	};
+
+	// setup
+	await prisma.exercise.createMany({
+		data: [{ name: exerciseNames[0] }, { name: exerciseNames[1] }]
+	});
+
+	// start a workout
+	await page.goto('/');
+	await page.locator('button[aria-label="startWorkout"]').click();
+
+	// assert exercise dropdown is visible
+	const exerciseDropdown = page.getByLabel('exerciseDropdown');
+	await expect(exerciseDropdown).toBeVisible();
+
+	// assert select-list options contain all exercises
+	const exercises = await prisma.exercise.findMany();
+	const dropdownOptions = page.getByTestId('exerciseDropdownItem');
+	expect(await dropdownOptions.count()).toEqual(exercises.length);
+
+	// add workout exercise
+	await expect(page.getByTestId('exerciseList')).not.toBeVisible();
+	await exerciseDropdown.selectOption(exerciseNames[0]);
+	await page.locator('button[aria-label="addExercise"]').click();
+	await expect(page.getByTestId('exerciseList')).toBeVisible();
+	const exerciseListItem = page.getByTestId('exerciseListItem');
+	await expect(exerciseListItem).toContainText(exerciseNames[0]);
+
+	// goto workout exercise
+	await exerciseListItem.click();
+
+	// add sets to exercise
+	await page.getByPlaceholder('Weight').fill(setInfo.weight);
+	await page.getByPlaceholder('Reps').fill(setInfo.reps);
+	await page.getByPlaceholder('RIR').fill(setInfo.rir);
+	await page.locator('button[aria-label="submitSet"]').click();
+
+	// assert set exists
+	const setListItem = page.getByTestId('setListItem');
+	await expect(setListItem).toBeVisible();
+	await expect(setListItem).toContainText(setInfo.weight);
+	await expect(setListItem).toContainText(setInfo.reps);
+	await expect(setListItem).toContainText(setInfo.rir);
+});
