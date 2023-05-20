3a344246179b32b1a60c522eb3eaa3074e26c1de
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-20 19:19:02 +0100

    reformat

--- src/routes/workout-exercises/[id]/+page.svelte
+++ src/routes/workout-exercises/[id]/+page.svelte
@@ -18,9 +18,9 @@
 		>
 			<div>{`${item.weight} x ${item.reps} x ${item.rir}`}</div>
 			<div class="flex flex-row">
-				<a href={`/sets/${item.id}/update`} class="btn btn-secondary" data-testid="updateSetBtn"
-					>Update</a
-				>
+				<a href={`/sets/${item.id}/update`} class="btn btn-secondary" data-testid="updateSetBtn">
+					Update
+				</a>
 				<form action={`?/delete`} method="POST" class="px-1" use:enhance>
 					<input type="hidden" name="id" value={item.id} />
 					<button class="btn btn-secondary" data-testid="deleteSetBtn">Delete</button>
