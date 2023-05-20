0bf47e26059f9f0c316ee5fed47ee854bdc1db20
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-20 20:28:40 +0100

    reduced code

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,5 @@
 import { prisma } from '$lib/server/prisma';
 
 export const load = async () => {
-	const exercises = await prisma.exercise.findMany();
-	return { exercises: exercises };
+	return { exercises: await prisma.exercise.findMany() };
 };
