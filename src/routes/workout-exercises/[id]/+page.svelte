56b4103615fe895462a63b35a9d81bd24647aa33
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-20 19:30:22 +0100

    changed listitem

--- src/routes/workout-exercises/[id]/+page.svelte
+++ src/routes/workout-exercises/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import ListItem from '$lib/components/ListItem.svelte';
 	import SetsForm from '$lib/components/SetsForm.svelte';
 	export let data;
 </script>
@@ -12,20 +13,16 @@
 
 <ul>
 	{#each data.workoutExercise.workoutExerciseSets as item}
-		<li
-			class="bg-neutral hover:bg-neutral-focus p-4 m-3 flex flex-row justify-between items-center"
-			data-testid="setListItem"
-		>
-			<div>{`${item.weight} x ${item.reps} x ${item.rir}`}</div>
-			<div class="flex flex-row">
-				<a href={`/sets/${item.id}/update`} class="btn btn-secondary" data-testid="updateSetBtn">
-					Update
-				</a>
+		<ListItem href={`/sets/${item.id}/update`} testId="setListItem">
+			<div class="flex flex-row justify-between items-center">
+				<div>{`${item.weight} x ${item.reps} x ${item.rir}`}</div>
 				<form action={`?/delete`} method="POST" class="px-1" use:enhance>
 					<input type="hidden" name="id" value={item.id} />
-					<button class="btn btn-secondary" data-testid="deleteSetBtn">Delete</button>
+					<button on:click|stopPropagation class="btn btn-secondary" data-testid="deleteSetBtn">
+						Delete
+					</button>
 				</form>
 			</div>
-		</li>
+		</ListItem>
 	{/each}
 </ul>
