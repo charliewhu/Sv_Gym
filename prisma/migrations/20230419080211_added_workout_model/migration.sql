1b8e7149a5f5304ba71a7291608843683f69e076
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-21 13:51:00 +0100

    reset prisma migrations

--- prisma/migrations/20230419080211_added_workout_model/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Workout" (
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "Workout_pkey" PRIMARY KEY ("id")
-);
---
+++ prisma/migrations/20230521125018_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "Exercise" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Exercise_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Workout" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Workout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkoutExercise" (
+    "id" SERIAL NOT NULL,
+    "workout" INTEGER NOT NULL,
+    "exercise" INTEGER NOT NULL,
+
+    CONSTRAINT "WorkoutExercise_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkoutExerciseSet" (
+    "id" SERIAL NOT NULL,
+    "workoutExercise" INTEGER NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "reps" INTEGER NOT NULL,
+    "rir" INTEGER NOT NULL,
+
+    CONSTRAINT "WorkoutExerciseSet_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "WorkoutExercise" ADD CONSTRAINT "WorkoutExercise_workout_fkey" FOREIGN KEY ("workout") REFERENCES "Workout"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkoutExercise" ADD CONSTRAINT "WorkoutExercise_exercise_fkey" FOREIGN KEY ("exercise") REFERENCES "Exercise"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkoutExerciseSet" ADD CONSTRAINT "WorkoutExerciseSet_workoutExercise_fkey" FOREIGN KEY ("workoutExercise") REFERENCES "WorkoutExercise"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
