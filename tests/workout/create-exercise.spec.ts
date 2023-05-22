dc6d84e1499347aa15a6e8e37a0136de1700eed3
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-22 08:47:46 +0100

    wrote test cases

--- tests/workout/create-exercise.spec.ts
+++ tests/workout/create-exercise.spec.ts
@@ -2,6 +2,9 @@ import { test, expect } from '../base.ts';
 import { prisma } from '../../src/lib/server/prisma.ts';
 
 test('create exercise from workout page', async ({ page }) => {
+	// arrange: variables
+	const exerciseName = 'Test Exercise';
+
 	// arrange (add workout exercise to db)
 	const workout = await prisma.workout.create({ data: {} });
 
@@ -9,9 +12,19 @@ test('create exercise from workout page', async ({ page }) => {
 	await page.goto(`/workouts/${workout.id}`);
 
 	// act: click create exercise btn
+	await page.locator('button', { hasText: 'Create New Exercise' }).click();
+
 	// assert: on create exercise page
+	await expect(page).toHaveTitle('Create Exercise');
+
 	// act: fill exercise name
-	// act: submit
+	await page.getByPlaceholder('Name').fill(exerciseName);
+	await page.locator('button[aria-label="submitNewExercise"]').click();
+
 	// assert: back on workout page
+	await expect(page).toHaveTitle(`Workout ${workout.id}`);
+
 	// assert: exercise appears in dropdown
+	const dropdownOptions = page.getByTestId('exerciseDropdownItem');
+	await expect(dropdownOptions).toContainText(exerciseName);
 });
