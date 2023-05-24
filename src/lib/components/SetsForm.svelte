1f18bc91ea41f7bdfdca695f1b4a8829313b6d69
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-24 17:56:33 +0100

    styling

--- src/lib/components/SetsForm.svelte
+++ src/lib/components/SetsForm.svelte
@@ -9,27 +9,22 @@
 </script>
 
 <form {action} method="POST" use:enhance class="text-center">
-	<div class="flex justify-evenly">
-		<div class="pb-4">
-			<div>
-				<label class="label" for="weight">
-					<span class="label-text">Weight</span>
-				</label>
-				<input
-					type="number"
-					inputmode="decimal"
-					step="0.5"
-					name="weight"
-					placeholder="Weight"
-					class="input input-bordered w-full max-w-xs flex flex-col"
-					value={form.weight === 0 ? null : form.weight}
-				/>
-			</div>
-			{#if errors?.weight}
-				<small>{errors.weight[0]}</small>
-			{/if}
+	<div class="flex justify-evenly mb-4">
+		<div>
+			<label class="label" for="weight">
+				<span class="label-text">Weight</span>
+			</label>
+			<input
+				type="number"
+				inputmode="decimal"
+				step="0.5"
+				name="weight"
+				placeholder="Weight"
+				class="input input-bordered w-full max-w-xs flex flex-col"
+				value={form.weight === 0 ? null : form.weight}
+			/>
 		</div>
-		<div class="pb-4">
+		<div>
 			<label class="label" for="reps">
 				<span class="label-text">Reps</span>
 			</label>
@@ -42,7 +37,7 @@
 				value={form.reps === 0 ? null : form.reps}
 			/>
 		</div>
-		<div class="pb-4">
+		<div>
 			<label class="label" for="rir">
 				<span class="label-text">RIR</span>
 			</label>
@@ -56,11 +51,16 @@
 			/>
 		</div>
 	</div>
-	{#if errors?._errors}
-		{#each errors._errors as error}
-			<small class="text-error">{error}</small>
-		{/each}
-	{/if}
+	<div class="flex flex-col mb-4">
+		{#if errors?._errors}
+			{#each errors._errors as error}
+				<small class="text-error">{error}</small>
+			{/each}
+		{/if}
+		{#if errors?.weight}
+			<small class="text-error">{errors.weight[0]}</small>
+		{/if}
+	</div>
 	<div>
 		<button aria-label={label} class="btn btn-primary w-10/12">
 			{text}
