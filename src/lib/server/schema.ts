8f3f64302eb7dcb1e01296a9594f700528d92775
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-24 17:56:52 +0100

    added gte validations

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 export const workoutExerciseSetSchema = z
 	.object({
-		weight: z.number(),
-		reps: z.number(),
-		rir: z.number()
+		weight: z.number().gte(0, { message: 'Weight must not be negative' }),
+		reps: z.number().gt(0, { message: 'Reps must not be negative' }),
+		rir: z.number().gte(0, { message: 'RIR must not be negative' })
 	})
 	.superRefine((val, ctx) => {
 		if (Number(!!val.weight) + Number(!!val.reps) + Number(!!val.rir) < 2) {
