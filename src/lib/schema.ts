e3e547421aa4b0825d6804e953d160eeafbac714
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-26 17:29:52 +0100

    changed optional to nullable

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -2,13 +2,13 @@ import { z } from 'zod';
 
 export const workoutExerciseSetSchema = z
 	.object({
-		weight: z.number().nonnegative({ message: 'Weight must not be negative' }).optional(),
+		weight: z.number().nonnegative({ message: 'Weight must not be negative' }).nullable(),
 		reps: z
 			.number()
 			.nonnegative({ message: 'Reps must not be negative' })
 			.int({ message: 'Reps must be a whole number' })
-			.optional(),
-		rir: z.number().nonnegative({ message: 'RIR must not be negative' }).optional()
+			.nullable(),
+		rir: z.number().nonnegative({ message: 'RIR must not be negative' }).nullable()
 	})
 	.superRefine((val, ctx) => {
 		if (Number(!!val.weight) + Number(!!val.reps) + Number(!!val.rir) < 2) {
