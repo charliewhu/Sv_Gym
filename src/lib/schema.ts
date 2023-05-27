16d2bcdb8d4a40ae7f346709ab4f93aec9894dad
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-27 07:06:56 +0100

    reps under 50

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -7,6 +7,7 @@ export const workoutExerciseSetSchema = z
 			.number()
 			.nonnegative({ message: 'Reps must be positive' })
 			.int({ message: 'Reps must be a whole number' })
+			.max(4, { message: 'Reps must be less than 50' })
 			.nullable(),
 		rir: z
 			.number()
