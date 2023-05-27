7205db68a9fa6d60d7acc8799ba1511f4b62ae5d
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-27 13:04:27 +0100

    added form action

---
+++ src/routes/routines/+page.server.ts
@@ -0,0 +1,18 @@
+import { prisma } from '$lib/server/prisma';
+import { fail, redirect } from '@sveltejs/kit';
+
+export const actions = {
+	default: async ({ request }) => {
+		const form = await request.formData();
+		const data = Object.fromEntries(form);
+
+		let routine;
+		try {
+			routine = await prisma.routine.create({ data: { name: data.name } });
+		} catch (err) {
+			return fail(404, { message: 'Could not create routine' });
+		}
+
+		throw redirect(301, `/routines/${routine.name}`);
+	}
+};
