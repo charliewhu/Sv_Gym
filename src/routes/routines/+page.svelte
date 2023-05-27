e7cde69099c7670ed74592e6c18e214b0631e023
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-27 11:25:54 +0100

    added form input

--- src/routes/routines/+page.svelte
+++ src/routes/routines/+page.svelte
@@ -1,3 +1,14 @@
 <svelte:head>
 	<title>Routines</title>
 </svelte:head>
+
+<form method="POST" class="text-center mb-6">
+	<input
+		type="text"
+		name="name"
+		placeholder="Name"
+		class="input input-bordered w-full max-w-xs mb-2"
+	/>
+
+	<button type="submit" class="btn btn-primary">Create</button>
+</form>
