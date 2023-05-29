a3a20bffbaaac381cea51e274fde535d7e2afe61
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-29 14:21:12 +0100

    initial test spec

---
+++ tests/routines/start-workout.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '../base.ts';
+import { prisma } from '../../src/lib/server/prisma.ts';
+
+test('create routine', async ({ page }) => {
+	// arrange: variables
+	const exerciseName = 'Test Exercise';
+	const routineName = 'Test Routine Day';
+
+	// arrange: db
+	const exercise = await prisma.exercise.create({ data: { name: exerciseName } });
+	const routine = await prisma.routine.create({ data: { name: routineName } });
+	await prisma.routineExercise.create({ data: { routine: routine.id, exercise: exercise.id } });
+
+	// arrange: go to routines page
+	// act: click routine workout button
+	// assert: on workout page
+	// assert: exercise from routine is in the list
+});
