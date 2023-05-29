929b8e6b34f8dda06264b36da635d856b4f634bb
Charlie Avery <charlieavery123@hotmail.co.uk>
2023-05-29 08:20:28 +0100

    added exercises dropdown

--- src/routes/routines/[name]/+page.svelte
+++ src/routes/routines/[name]/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import ExerciseDropdown from '$lib/components/ExerciseDropdown.svelte';
+
 	export let data;
 </script>
 
 <svelte:head>
 	<title>{data.routine.name}</title>
 </svelte:head>
+
+<ExerciseDropdown action="" exercises={data.exercises} />
